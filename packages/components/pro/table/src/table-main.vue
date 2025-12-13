5f795a8d7ee650bbffcc73519693e66ee32858fe
kele_Bingtang <2456019588@qq.com>
2025-12-14 01:36:59 +0800

    fix(table): 🐞 调整超级表格表头背景色样式，修复透明问题

--- packages/components/pro/table/src/table-main.vue
+++ packages/components/pro/table/src/table-main.vue
@@ -313,7 +313,7 @@ defineExpose(expose);
     ref="elTableInstance"
     show-overflow-tooltip
     v-bind="$attrs"
-    :header-cell-style="{ backgroundColor: `rgb(${ns.cssVar('gray-200-rgb')}, 0.6)`, ...headerCellStyle }"
+    :header-cell-style="{ backgroundColor: ns.cssVar('gray-200'), ...headerCellStyle }"
     :data="tableData"
     :row-key
     :class="ns.b()"
